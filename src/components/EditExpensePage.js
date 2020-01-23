0013df3821eb95d73b8c4273dde5b3a7c05b1bd0
Miras Kenzhegaliyev <kenzhemir@gmail.com>
2020-01-23 16:03:08 +0200

    Break down code into separate files

---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const EditExpensePage = props => {
+	console.log(props);
+	return <div>Editing expense with param id = {props.match.params.id}</div>;
+};
+
+export default EditExpensePage;
