d7f656b819b8918a2cdb9f8081e72db8669a86f8
Miras Kenzhegaliyev <kenzhemir@gmail.com>
2020-01-24 14:01:25 +0200

    Connect redux to the application

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
 import "normalize.css";
 import "./styles/styles.scss";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
+
+const store = configureStore();
+
+store.dispatch(addExpense({ amount: 100, description: "Water bill" }));
+store.dispatch(addExpense({ amount: 300, description: "Gas bill" }));
+store.dispatch(setTextFilter("gas"));
+
+const state = store.getState();
+console.log(getVisibleExpenses(state.expenses, state.filters));
 
 ReactDOM.render(<AppRouter />, document.getElementById("app"));
---
+++ src/reducers/expenses.js
@@ -0,0 +1,15 @@
+const expensesReducerDefaultState = [];
+export default (state = expensesReducerDefaultState, action) => {
+	switch (action.type) {
+		case "ADD_EXPENSE":
+			return [...state, action.expense];
+		case "REMOVE_EXPENSE":
+			return state.filter(({ id }) => id != action.id);
+		case "EDIT_EXPENSE":
+			return state.map(expense =>
+				expense.id == action.id ? { ...expense, ...action.updates } : expense
+			);
+		default:
+			return state;
+	}
+};
