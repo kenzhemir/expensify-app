5d112f73c6b1385a6458c9a05f6894a4f3aab88d
Miras Kenzhegaliyev <kenzhemir@gmail.com>
2020-01-27 00:58:22 +0200

    Test ExpenseListFilters's methods

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import { ExpenseListFilters } from "../../components/ExpenseListFilters";
 import { filters, altFilters } from "../fixtures/filters";
+import moment from "moment";
 
 let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper;
 
@@ -29,4 +30,46 @@ test("should render ExpenseListFilters correctly", () => {
 
 test("should render ExpenseListFilters with alt filters correctly", () => {
 	expect(wrapper.setProps({ filters: altFilters })).toMatchSnapshot();
+});
+
+test("should handle text change", () => {
+	const value = "some input";
+	wrapper.find("input").prop("onChange")({ target: { value } });
+	expect(setTextFilter).toHaveBeenLastCalledWith(value);
+});
+
+test("should sort by date", () => {
+	const value = "date";
+	wrapper.setProps({ filters: altFilters });
+	wrapper.find("select").prop("onChange")({ target: { value } });
+	expect(sortByDate).toHaveBeenCalled();
+});
+
+test("should sort by amount", () => {
+	const value = "amount";
+	wrapper.find("select").prop("onChange")({ target: { value } });
+	expect(sortByAmount).toHaveBeenCalled();
+});
+
+test("should handle date changes", () => {
+	const startDate = moment(0).add(3, "years");
+	const endDate = moment(0).add(5, "years");
+	wrapper.find("withStyles(DateRangePicker)").prop("onDatesChange")({
+		startDate,
+		endDate
+	});
+	expect(setStartDate).toHaveBeenLastCalledWith(startDate);
+	expect(setEndDate).toHaveBeenLastCalledWith(endDate);
+});
+
+test("should handle date focus changes", () => {
+	const onFocusChange = wrapper
+		.find("withStyles(DateRangePicker)")
+		.prop("onFocusChange");
+	onFocusChange("startDate");
+	expect(wrapper.state("calendarFocused")).toBe("startDate");
+	onFocusChange("endDate");
+	expect(wrapper.state("calendarFocused")).toBe("endDate");
+	onFocusChange(null);
+	expect(wrapper.state("calendarFocused")).toBe(null);
 });
