cab7b83fab6407fccdf1302f8a85c5142c391741
Miras Kenzhegaliyev <kenzhemir@gmail.com>
2020-01-27 21:30:02 +0200

    Make webpack production friendly

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,41 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-	mode: "development",
-	entry: "./src/app.js",
-	output: {
-		path: path.join(__dirname, "public"),
-		filename: "bundle.js"
-	},
-	module: {
-		rules: [
-			{
-				loader: "babel-loader",
-				test: /\.js$/,
-				exclude: /node_modules/
-			},
-			{
-				test: /\.s?css$/,
-				use: ["style-loader", "css-loader", "sass-loader"]
-			}
-		]
-	},
-	devtool: "cheap-module-eval-source-map",
-	devServer: {
-		contentBase: path.join(__dirname, "public"),
-		historyApiFallback: true
-	}
+module.exports = env => {
+	const isProduction = env === "production";
+	return {
+		plugins: [
+			new MiniCssExtractPlugin({
+				filename: "styles.css"
+			})
+		],
+		mode: "development",
+		entry: "./src/app.js",
+		output: {
+			path: path.join(__dirname, "public"),
+			filename: "bundle.js"
+		},
+		module: {
+			rules: [
+				{
+					loader: "babel-loader",
+					test: /\.js$/,
+					exclude: /node_modules/
+				},
+				{
+					test: /\.s?css$/,
+					use: [
+						MiniCssExtractPlugin.loader,
+						{ loader: "css-loader", options: { sourceMap: true } },
+						{ loader: "sass-loader", options: { sourceMap: true } }
+					]
+				}
+			]
+		},
+		devtool: isProduction ? "source-map" : "inline-source-map",
+		devServer: {
+			contentBase: path.join(__dirname, "public"),
+			historyApiFallback: true
+		}
+	};
 };
